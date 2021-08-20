4fe3e852c7568ef0ec509a9e085ee49622f1b1f5
WilmerE <wilmerpelico22@gmail.com>
2021-08-20 17:50:49 -0600

    Lista de registros de evlenguaje

--- ajax/ev_lenguaje_list.php
+++ ajax/ev_lenguaje_list.php
@@ -5,7 +5,7 @@ include '../db_connection.php';
 $conn = OpenCon();
 
 //Consulta a la tabla usuario
-$consulta = "SELECT * FROM tvx_form_evlenguaje";
+$consulta = "SELECT form.id, u.display_name, u.user_login, u.user_email FROM tvx_form_evlenguaje form INNER JOIN tvx_users u ON u.ID=form.idpaciente";
 $resultado = $conn->query($consulta);
 
 //RECIBIMOS EL REQUEST DE AJAX
@@ -13,22 +13,22 @@ if (isset($_POST['action'])) {
 	if(!$resultado){
 		die("No hay datos para mostrar");
 	} else {
-		$data = array(); 
+		$i = 0;
 		while( $row = mysqli_fetch_array($resultado) ){
 			$data[] = array(
-				'numero' => $row["numero"],
+				'numero' => $row["id"],
 				'nombre' => $row["display_name"],
 				'usuario' => $row["user_login"],
-				'telefono' => $row["telefono"],
 				'mail' => $row["user_email"],
 			);
 		}
+
 		$datatable=array(
 			"draw"=>1,
 			"recordsTotal"=>count($data),
 			"recordsFiltered"=>count($data),
 			"data"=>$data
-		); 
+		);
 		echo json_encode($datatable);
 	}
 }
